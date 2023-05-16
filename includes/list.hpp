8b08e681cd6199524e7896409376c500c14c1fa6
shayan <s.shayan.sohail@gmail.com>
2023-05-16 23:38:18 +0500

    Removed Iterator traits, will add in the end, added print and add_front in List

--- includes/list.hpp
+++ includes/list.hpp
@@ -3,58 +3,27 @@
 
 enum class ListType { Single, Double, Circular };
 
-template <typename T>
-class ListIterator {
+template<typename T>
+class Node
+{
 public:
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type = std::ptrdiff_t;
-    using pointer = T*;
-    using reference = T&;
-
-    ListIterator(T* ptr) : m_ptr(ptr) {}
-    ListIterator(const ListIterator& other) : m_ptr(other.m_ptr) {}
-
-    ListIterator& operator++()
-    {
-        m_ptr++;
-        return *this;
-    }
-
-    ListIterator operator++(int) {
-        ListIterator tmp(*this);
-        operator++();
-        return tmp;
-    }
-
-    bool operator==(const ListIterator& other) const {
-        return m_ptr == other.m_ptr;
-    }
-
-    bool operator!=(const ListIterator& other) const {
-        return m_ptr != other.m_ptr;
-    }
-
-    reference operator*() const {
-        return *m_ptr;
-    }
-
-    pointer operator->() const {
-        return m_ptr;
-    }
-
-private:
-    T* m_ptr;
+    T data;
+    Node* next;
+    Node(T value) : data(value), next(nullptr) {}
 };
+
 
 /*List Interface*/
 template <typename T>
-class ListBase {
-
+class ListBase
+{
 protected:
     size_t m_size;
+    Node<T>* m_head;
+    Node<T>* m_tail; /*One past the last element*/
 
 public:
-    ListBase() : m_size(0) {}
+    ListBase() : m_size(0), m_head(nullptr), m_tail(nullptr) {}
 
     virtual void add_front(T value) = 0;
     virtual void add_at(T value, size_t index) = 0;
@@ -64,12 +33,7 @@ public:
     virtual void remove_at(T value, size_t index) = 0;
     virtual void remove_back(T value) = 0;
 
-    virtual ListIterator<T> begin() const = 0;
-    virtual ListIterator<T> end() const = 0;
-
-    using value_type = T;
-    using iterator = ListIterator<T>;
-    using const_iterator = ListIterator<const T>;
+    virtual void clear() = 0;
 };
 
 /*Generic Class with enum templated argument*/
@@ -77,11 +41,23 @@ template <typename T, ListType LT = ListType::Single>
 class List : public ListBase<T> {
 
 public:
-    List() {
+    List()
+    {
         std::cout << "Creating List of type " << static_cast<int>(LT) << std::endl;
     }
+    ~List()
+    {
+        this->clear();
+    }
 
-    void add_front(T value) override { std::cout << "Generic add_front" << std::endl; }
+    void add_front(T value) override
+    {
+        this->m_size++;
+        Node<T>* temp = new Node<T>(value);
+        temp->next = this->m_head;
+        this->m_head = temp;
+    }
+
     void add_at(T value, size_t index) override {}
     void add_back(T value) override {}
 
@@ -89,17 +65,31 @@ public:
     void remove_at(T value, size_t index) override {}
     void remove_back(T value) override {}
 
-    ListIterator<T> begin() const override {
-        // Return an iterator to the beginning of the list
-        return ListIterator<T>(nullptr);
+    void print()
+    {
+        std::cout << "Size is " << this->m_size << std::endl;
+        Node<T>* it = this->m_head;
+        while (it != this->m_tail)
+        {
+            std::cout << it->data << std::endl;
+            it = it->next;
+        }
     }
-
-    ListIterator<T> end() const override {
-        // Return an iterator to the end of the list
-        return ListIterator<T>(nullptr);
+    void clear() override
+    {
+        Node<T>* it = this->m_head;
+        while (it != this->m_tail)
+        {
+            Node<T>* temp = it;
+            it = it->next;
+            delete temp;
+            this->m_size--;
+        }
+        this->m_head = nullptr;
     }
 };
 
+/*
 template <typename T>
 class List<T, ListType::Double>
 {
@@ -119,9 +109,17 @@ public:
         std::cout << "Adding " << value << " to Circular List" << std::endl;
     }
 };
-
+*/
 int main()
 {
     List<int> m_List;
-    m_List.add_back(5);
+    m_List.add_front(5);
+    m_List.add_front(4);
+    m_List.add_front(3);
+    m_List.add_front(2);
+    m_List.add_front(1);
+    m_List.print();
+    m_List.clear();
+    m_List.print();
+
 }
